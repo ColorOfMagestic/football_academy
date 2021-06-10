7aa2a55a92cf203cd6826a7497ddccfcbf667524
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2021-06-10 16:33:42 +0500

    Added "feedback-slider"

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,14 @@ const swiper = new Swiper('.coach-slider', {
   }
 });
 
+//Slider feedback
+new Swiper('.feedback-slider', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+
