c961c3559578841b2c831ed8a9a2bb3505b7bc67
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2021-06-10 13:58:07 +0500

    changes in the slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,13 @@
 
+//Slider coach
 const swiper = new Swiper('.coach-slider', {
+  observeParents: true,
+  observer: true,
+  simulateTouch: false,
 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
 });
+
