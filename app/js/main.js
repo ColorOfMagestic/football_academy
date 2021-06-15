bfb3ea879fb905d902efdf50175ec1472671887d
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2021-06-15 21:17:47 +0500

    Added "photo-slider"

--- app/js/main.js
+++ app/js/main.js
@@ -25,8 +25,17 @@ new Swiper('.cafe-slider', {
   loop: true,
   spaceBetween: 100,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.cafe-slider_button-next',
+    prevEl: '.cafe-slider_button-prev',
+  }
+});
+//Slider photo
+new Swiper('.photo-slider', {
+  observeParents: true,
+
+  navigation: {
+    nextEl: '.photo-slider_button-next',
+    prevEl: '.photo-slider_button-prev',
   }
 });
 
